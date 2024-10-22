3b5417a48fd08fc8f6c0624bbd4ab60ebf51e5da
joshua041904 <joshuaswaida@gmail.com>
2024-10-22 16:49:54 -0400

    Ran npm install and created file for run button

---
+++ codebase/src/components/RunAlgoButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click="runAlgorithm">Run</button>
+</template>
+  
+  <script>
+  export default {
+    methods: {
+      runAlgorithm() {
+        alert('Algorithm running!');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  button {
+    background-color: green;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  </style>
