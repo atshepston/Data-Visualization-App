94b00cb9b7374165dd3ad0c62bba7489efe29c8b
joshua041904 <joshuaswaida@gmail.com>
2024-10-24 13:56:20 -0400

    Deleted unnecessary files (base.css, main.css, logo.svg). Cleared default code in App.vue. Created folder for sorting (with sub folders for algorithm code and visual components).

---
+++ codebase/src/sorting/components/main.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="page">
+        <h1>Array Sorting</h1>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+  
+<style scoped>
+  div {
+    width: 100%;
+    height: 100%;
+    background-color: blue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  h1 {
+    margin: 0;
+    color: white;
+  }
+  </style>
